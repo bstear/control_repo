1b935b481cec56e411c594b2c8b297a0491a5c0e
bstear <45983686+bstear@users.noreply.github.com>
2018-12-20 17:55:34 -0800

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDOEG8EPukSiMNiGWruVpLm0QQEXCxhy97awoPhRqVmlY151lJpK+S7PKIjJIaHunGz1Xi4Z/ZnTvHhyEBrl8ChkeSoG45W9a0TnHZ81wOd5cJmAkTQBk1zro4apIUfYp+1XzuCivDPDv8S47J8QaPy+/qlSaBVoba08ezLFk2UOwGS7Q0jiG/+WyYR8iM7U+OtcrNGwuIT5V4l+aMKpo3euSHgKpGd6uJyc00K8AX8ulebK/r8YP3UNXaGPB3vACgMpO7QuSro7soAuRK48Wsj+h8yAiKDcfTy9ZUJkNYApAKmpGd8NLsYVZQ3OxmyYV5IvdpSF/jZJX8QnhFbsP7Z',
 	}  
 }
