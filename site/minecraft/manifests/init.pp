7c6459392090a5295f57e3b307eae84f536047c3
bstear <45983686+bstear@users.noreply.github.com>
2019-01-03 16:43:04 -0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,24 +1,24 @@
 class minecraft {
-  file {'/opt/minecraft':
+  file {‘/opt/minecraft’:
     ensure => directory,
-   }
-   file {'/opt/minecraft/minecraft_server.jar':
-   ensure => file,
-   source => 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
-   }
-   package => {'java':
+  }
+  file {‘/opt/minecraft/minecraft_server.jar’:
+    ensure => file,
+    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+  }
+  package {‘java’:
     ensure => present,
-   }
-   file {'/opt/minecraft/eula.txt':
+  }
+  file {‘/opt/minecraft/eula.txt’:
     ensure => file,
-    content => 'eula=true',
-   }
-   file {'/etc/systemd/system/minecraft.service':
+    content => ‘eula=true’
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.servce',
-   }
-   service {'minecraft':
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
+  }
+  service { ‘minecraft’:
     ensure => running,
     enable => true,
-   }
-}   
+  }
+}
